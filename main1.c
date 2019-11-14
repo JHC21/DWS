d490240619b4625208c5bdee9535c3744e908aed
JHC21 <comma1010k@gmail.com>
2019-11-14 11:58:06 +0900

    add time structure

--- main1.c
+++ main1.c
@@ -13,45 +13,52 @@ int main(int argc, const char * argv[]) {
     char status,isSet;
     bool isAlarm = false;
 
-    time_t currentTime, alarmTime;
+    struct tm currentTime, alarmTime;
+    time_t currentTime_s, alarmTime_s;
     double measureTime, lapTime;
     clock_t sw_start, sw_end;
     clock_t timer_start, timer_end;
 
     /* Initialize */
     status = TKMODE;
-    time(&currentTime);
+
+    currentTime.tm_year = 2019 - 1900;
+    currentTime.tm_mon = 0; // start from 0
+    currentTime.tm_mday = 1;
+    currentTime.tm_hour = 0;
+    currentTime.tm_min = 0;
+    currentTime.tm_sec = 0;
+    currentTime.tm_isdst = 0; // no summer time
+
     measureTime = 0;
     lapTime = 0;
 
     /* start DWS */
+    timer_start = clock();
     while(true) {
-
-        timer_start = clock();
-
-        alarmCheck(currentTime, alarmTime);
-        if(!isAlarm) {
-            if(alarmIndicator){ /* if alarm indicator is On, check Alarm time */
-                isAlarm = alarmCheck(currentTime, alarmTime);
-            }
-            memset(input,0,BUFSIZE);
-            if (kbhit() != 0) {
-               scanf("%s", input);
-            }
+       alarmCheck(currentTime, alarmTime);
+       if(!isAlarm) {
+          if(alarmIndicator){ /* if alarm indicator is On, check Alarm time */
+             isAlarm = alarmCheck(currentTime, alarmTime);
+          }
+          memset(input,0,BUFSIZE);
+          if (kbhit() != 0) {
+             scanf("%s", input);
+          }
   
-            determinePriority(input);
+          determinePriority(input);
 
-            if(input[0]==BUTTOND){
-                backlightCheck();
-            }else{
-                switch( status ){
-                    case TKMODE:
-                        if (input[0] == BUTTONC){
-                            status = ALMODE;
-                        }
+          if(input[0]==BUTTOND){
+             backlightCheck();
+          }else{
+             switch( status ){
+                case TKMODE:
+                   if (input[0] == BUTTONC){
+                      status = ALMODE;
+                   }
 
-			display();
-                        break;
+		   display();
+                   break;
 
                     case STCURTIME: /* set current time */
                         if (isSet == status) {
@@ -229,6 +236,26 @@ int main(int argc, const char * argv[]) {
             }
         }
         timer_end = clock();
+	if ((double) (timer_end - timer_start) / CLOCKS_PER_SEC > 1){
+	   timer_start = clock();
+
+ 	   currentTime.tm_sec++;
+	   
+	   if(currentTime.tm_sec>60){
+	      currentTime.tm_min++;
+	      currentTime.tm_sec-=60;
+
+	      if(currentTime.tm_min>60){
+	         currentTime.tm_hour++;
+		 currentTime.tm_min-=60;
+
+		 if(currentTime.tm_hour>24){
+		    currentTime.tm_day++;
+                    currentTime.tm_hour-=24;
+		 }
+	      }
+	   }
+	}	
     }
     return 0;
 }
